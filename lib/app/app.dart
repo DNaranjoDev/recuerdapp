326f355762ead41e605f76cc5cda265f839c2de7
AlexSpinola <alex.spinola.ss@gmail.com>
2023-04-09 01:56:54 +0100

    Colors

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,9 +4,15 @@ import 'package:recuerdapp/app/view/my_home_page/my_home_page.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    const Color primary =  Color(0xFF181A20);
+    const Color textColor =  Color(0xFFFFFFFF);
+    const Color backgroundColor =  Color(0xFF181A20);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'RecuerdApp',
